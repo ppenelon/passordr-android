ce055a2ef16b0b53da7ba2d2817b0396dea7bb25
Pierre Penelon <penelon.pierre@gmail.com>
2025-10-02 00:38:34 +0200

    Add TextField

--- app/src/main/java/com/ppenelon/passordr/MainActivity.kt
+++ app/src/main/java/com/ppenelon/passordr/MainActivity.kt
@@ -1,23 +1,31 @@
 package com.ppenelon.passordr
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ppenelon.passordr.ui.theme.AppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -35,7 +43,7 @@ class MainActivity : ComponentActivity() {
                                 titleContentColor = MaterialTheme.colorScheme.primary,
                             ),
                             title = {
-                                Text("Small Top App Bar")
+                                Text("Passordr")
                             }
                         )
                     },
@@ -53,16 +61,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.LightGray) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp),
-            color = Color.Red
-        )
+    var username by remember { mutableStateOf(name) }
+
+    Surface(color = MaterialTheme.colorScheme.background, modifier = Modifier.fillMaxWidth(1f).padding(4.dp)) {
+        Column {
+            Text(
+                text = "Hello $username!",
+                fontSize = 30.sp,
+                modifier = modifier.padding(4.dp)
+            )
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Your name") }
+            )
+        }
     }
 }
 
 @Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun GreetingPreview() {
     AppTheme {
